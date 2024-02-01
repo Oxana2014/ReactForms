bc6a194ccea451ab834c03841ffbea88f04674e9
Oksana Kormilina <keysb@mail.ru>
2024-02-01 13:33:15 +0400

    Add validation upon submission

--- src/components/RefLogin.jsx
+++ src/components/RefLogin.jsx
@@ -1,18 +1,24 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
 
-const email = useRef()
-const password=useRef()
+  const email = useRef();
+  const password = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
-    const enteredEmail = email.current.value
-    const enteredPassword = password.current.value
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
     console.log(enteredEmail, enteredPassword);
-   // email.current.value = ''
-   //less code with 
-   event.target.reset()
+
+    const emailIsValid = enteredEmail.includes("@");
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+    setEmailIsInvalid(false);
+    console.log("Sending http request...");
   }
 
   return (
@@ -22,7 +28,10 @@ const password=useRef()
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" ref={email}/>
+          <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
@@ -32,7 +41,7 @@ const password=useRef()
       </div>
 
       <p className="form-actions">
-        <button className="button button-flat">Reset</button>
+        <button type='reset' className="button button-flat">Reset</button>
         <button onClick={handleSubmit} className="button">
           Login
         </button>
