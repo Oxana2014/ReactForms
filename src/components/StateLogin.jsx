163f183019ec4a987279c0726df0ce68504ab695
Oksana Kormilina <keysb@mail.ru>
2024-02-01 13:00:00 +0400

    Add validation on lost focus

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -7,20 +7,33 @@ export default function Login() {
     email: "",
     password: "",
   });
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
 
   function handleInputChange(identifier, value) {
     setEnteredValues((prevValues) => ({
       ...prevValues,
       [identifier]: value,
     }));
+    setDidEdit(prevEdit => ({...prevEdit, [identifier]: false}))
   }
-  const emailIsInvalid = enteredValues.email !== '' && !enteredValues.email.includes('@')
+  const emailIsInvalid =
+    didEdit.email && !enteredValues.email.includes("@");
 
   function handleSubmit(event) {
     event.preventDefault();
     console.log(enteredValues);
     // use enteredValues
     setEnteredValues({ email: "", password: "" });
+  }
+  function handleInputBlur(identifier) {
+    setDidEdit(prevEdit => {
+        return {...prevEdit, [identifier]: true}
+    }   
+    )
   }
 
   return (
@@ -34,11 +47,12 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
+            onBlur={() => handleInputBlur('email')}
             onChange={(event) => handleInputChange("email", event.target.value)}
             value={enteredValues.email}
           />
           <div className="control-error">
-{emailIsInvalid && <p>Please enter a valid email address</p>}
+            {emailIsInvalid && <p>Please enter a valid email address</p>}
           </div>
         </div>
 
